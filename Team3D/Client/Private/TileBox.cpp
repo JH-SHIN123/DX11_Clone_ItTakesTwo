00d89d4df290faf5369dbcc6524ed2de931a343c
DESKTOP-5LKNKDR\irmin <irmin95@gmail.com>
2021-11-19 15:53:44 +0900

    211119_04
    # 프로젝트 정리

--- Team3D/Client/Private/TileBox.cpp
+++ Team3D/Client/Private/TileBox.cpp
@@ -84,16 +84,16 @@ HRESULT CTileBox::Render()
 	m_pModelCom->Set_DefaultVariables_Shadow();
 	m_pModelCom->Render_Model(0, m_iRenderNum);
 
-	/* 유리창 렌더 코드 */
+	/* Window Render Code */
 	//_uint iRenderCount = m_pModelCom->Frustum_Culling();
 	//m_pModelCom->Bind_GBuffers(iRenderCount);
 
-	//// 유리창
+	//// Window
 	//m_pModelCom->Set_ShaderResourceView("g_DiffuseTexture", 0, aiTextureType_DIFFUSE, m_iRenderNum);
 	//m_pModelCom->Set_ShaderResourceView("g_NormalTexture", 0, aiTextureType_NORMALS, m_iRenderNum);
 	//m_pModelCom->Render_ModelByPass(iRenderCount, 0, 2);
 
-	//// 창틀
+	//// Window Frames
 	//m_pModelCom->Set_ShaderResourceView("g_DiffuseTexture", 1, aiTextureType_DIFFUSE, m_iRenderNum);
 	//m_pModelCom->Set_ShaderResourceView("g_NormalTexture", 1, aiTextureType_NORMALS, m_iRenderNum);
 	//m_pModelCom->Render_ModelByPass(iRenderCount, 1, 3);
