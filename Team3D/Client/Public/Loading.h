e7a481f58b4ad36b9a172eab0cff11f03118b707
DESKTOP-LEIKETP\BLUE <aldald1318@gmail.com>
2021-11-28 16:14:49 +0900

    MERGE WT - EMISSIVE BLUR

--- Team3D/Client/Public/Loading.h
+++ Team3D/Client/Public/Loading.h
@@ -10,7 +10,7 @@ BEGIN(Client)
 * 각자 CPU 상태보다 높은 상태는 주석처리
 * 로딩에 코드 추가할 때 16/8/6 스레드별 함수 모두 세팅할 것.
 */
-#define __MAPLOADING_OFF
+//#define __MAPLOADING_OFF
 //#define __6THREADS
 #define __8THREADS
 //#define __16THREADS
