a724b16b534817d5160a3da329138abd445423e3
DESKTOP-5LKNKDR\irmin <irmin95@gmail.com>
2021-11-29 00:02:18 +0900

    MAIN_211129_01
    # 중력발판 구현중

--- Team3D/Client/Private/PlayerBehaviorCallback.cpp
+++ Team3D/Client/Private/PlayerBehaviorCallback.cpp
@@ -14,8 +14,8 @@ PxControllerBehaviorFlags CPlayerBehaviorCallback::getBehaviorFlags(const PxShap
 			m_pPlayerActor->Reorder_Gravity();
 	}
 
-	if (CPhysX::GetInstance()->Raycast(MH_PxVec3(m_pController->getFootPosition()), MH_PxVec3(XMVectorSet(0.f, 1.f, 0.f, 0.f)), 0.5f, Buffer))
-		return PxControllerBehaviorFlag::eCCT_CAN_RIDE_ON_OBJECT;
+	//if (CPhysX::GetInstance()->Raycast(MH_PxVec3(m_pController->getFootPosition()), MH_PxVec3(XMVectorSet(0.f, 1.f, 0.f, 0.f)), 0.5f, Buffer))
+	//	return PxControllerBehaviorFlag::eCCT_CAN_RIDE_ON_OBJECT;
 
 	return PxControllerBehaviorFlag::eCCT_CAN_RIDE_ON_OBJECT | PxControllerBehaviorFlag::eCCT_SLIDE;
 }
