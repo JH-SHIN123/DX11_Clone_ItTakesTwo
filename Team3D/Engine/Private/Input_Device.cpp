025b87f012d38de39ad70e8c6b66859a44319515
DESKTOP-5LKNKDR\irmin <irmin95@gmail.com>
2021-12-07 19:50:13 +0900

    MAIN_211207_11
    # 내용 없음

--- Team3D/Engine/Private/Input_Device.cpp
+++ Team3D/Engine/Private/Input_Device.cpp
@@ -234,8 +234,11 @@ _int CInput_Device::Tick(_bool bWndActivate)
 		memcpy(m_byKeyState_Down, m_byKeyState_Down_Buffer, sizeof(_byte) * 256);
 		memcpy(&m_MouseState_Up, &m_MouseState_Up_Buffer, sizeof(DIMOUSESTATE));
 		memcpy(&m_MouseState_Down, &m_MouseState_Down_Buffer, sizeof(DIMOUSESTATE));
+
+#ifndef __DEBUGGING_PAD
 		memcpy(&m_JoyStickState_Up, &m_JoyStickState_Up_Buffer, sizeof(DIJOYSTATE));
 		memcpy(&m_JoyStickState_Down, &m_JoyStickState_Down_Buffer, sizeof(DIJOYSTATE));
+#endif 
 	}
 	else
 	{
