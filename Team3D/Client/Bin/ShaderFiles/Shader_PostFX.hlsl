b0f2b1bf88da8379bbecccb31dc3fa79e7df3d84
DESKTOP-LEIKETP\BLUE <aldald1318@gmail.com>
2021-12-09 13:34:11 +0900

    ㄷㅍㅌ

--- Team3D/Client/Bin/ShaderFiles/Shader_PostFX.hlsl
+++ Team3D/Client/Bin/ShaderFiles/Shader_PostFX.hlsl
@@ -34,8 +34,8 @@ cbuffer RadialBlurDesc
 	-0.2, -0.17, -0.14, -0.12, -0.09, -0.07, -0.05, -0.03, -0.02, -0.01,
 	0.01, 0.02, 0.03,0.05, 0.07,0.09,0.12, 0.14, 0.17, 0.2 };
 
-	float	g_SampleDist = 0.15f; // 0.15f
-	float	g_SampleStrength = 20.7f;
+	float	g_SampleDist = 0.2; // 0.15f
+	float	g_SampleStrength = 20.f;
 
 	bool	g_bRadiarBlur_Main = false;
 	bool	g_bRadiarBlur_Sub = false;
