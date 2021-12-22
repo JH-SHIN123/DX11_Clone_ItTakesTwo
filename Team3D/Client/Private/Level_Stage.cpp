d56c1a13e9fca8dcf7505ac09783f39dd05056f2
DESKTOP-5LKNKDR\irmin <irmin95@gmail.com>
2021-12-22 09:33:47 +0900

    MAIN_211222_병합

--- Team3D/Client/Private/Level_Stage.cpp
+++ Team3D/Client/Private/Level_Stage.cpp
@@ -131,13 +131,6 @@ _int CLevel_Stage::Tick(_double dTimedelta)
 	}*/
 #endif
 
-#ifdef __TEST_SE
-	if (m_pGameInstance->Key_Down(DIK_INSERT))
-		EFFECT->Add_Effect(Effect_Value::PipeLocker_Connected, XMMatrixTranslation(64.f, 1.f, 15.f));
-	if (m_pGameInstance->Key_Down(DIK_HOME))
-		EFFECT->Add_Effect(Effect_Value::Gate_Smoke, XMMatrixRotationY(XMConvertToRadians(90.f)) * XMMatrixTranslation(58.f, 1.f, 30.f));
-#endif
-
 	return NO_EVENT;
 }
 
