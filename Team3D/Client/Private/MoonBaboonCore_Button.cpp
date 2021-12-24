48a718e2842777e0d2c9fa55bbcc160d16564686
DESKTOP-LEIKETP\BLUE <aldald1318@gmail.com>
2021-12-24 10:59:56 +0900

    문바본 코어 버튼 활성화 이미시브 적용

--- Team3D/Client/Private/MoonBaboonCore_Button.cpp
+++ Team3D/Client/Private/MoonBaboonCore_Button.cpp
@@ -89,7 +89,7 @@ HRESULT CMoonBaboonCore_Button::Render(RENDER_GROUP::Enum eGroup)
 	NULL_CHECK_RETURN(m_pModelCom, E_FAIL);
 	m_pModelCom->Set_DefaultVariables_Perspective(m_pTransformCom->Get_WorldMatrix());
 	m_pModelCom->Set_DefaultVariables_Shadow();
-	m_pModelCom->Render_Model(1);
+	m_pModelCom->Render_Model(m_iRenderPass); // Active : 36, Non - Active 37
 
 	return S_OK;
 }
