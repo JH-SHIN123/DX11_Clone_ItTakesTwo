38511460b4eda92022d38d789f1ea5bb740de46c
hye10605@naver.com <hye10605@naver.com>
2021-12-26 17:40:25 +0900

    MERGE_211226_03_Hyewon
    # 오브젝트 컷씬에 맞춰서 재배치
    
    # 엔딩 크레딧 화이트아웃 적용

--- Team3D/Client/Public/GameOption.h
+++ Team3D/Client/Public/GameOption.h
@@ -2,8 +2,8 @@
  
 /************************************************************************/
 
-//#define __FULLSCREEN
-//#define __START_LEVEL_LOGO
+#define __FULLSCREEN
+#define __START_LEVEL_LOGO
 #define __PLAY_CUTSCENE
 #define __FIRST_CUTSCENE_OFF
     
@@ -50,7 +50,7 @@
 /**
 * 메이 키보드로 컨트롤하려면 주석 해제.
 */
-#define __CONTROL_MAY_KEYBOARD
+//#define __CONTROL_MAY_KEYBOARD
 
 /************************************************************************/
 
@@ -63,7 +63,7 @@
 //#define __TEST_TAEK
 //#define __TEST_YOON
 //#define __TEST_JIN
-#define __TEST_JUN
+//#define __TEST_JUN
 
 /************************************************************************/
 
