0d4ed14c6214dd5fe43470f0eaad107536e536e8
qorxmf <qorxmf@naver.com>
2021-12-27 19:07:04 +0900

    중력자 폭탄 플레이어 데미지 달도록 구현

--- Team3D/Client/Private/Effect_Boss_Gravitational_Bomb.cpp
+++ Team3D/Client/Private/Effect_Boss_Gravitational_Bomb.cpp
@@ -4,6 +4,9 @@
 #include "Effect_Boss_Gravitational_Bomb_Particle.h"
 #include "Effect_Generator.h"
 
+#include "Cody.h"
+#include "May.h"
+
 CEffect_Boss_Gravitational_Bomb::CEffect_Boss_Gravitational_Bomb(ID3D11Device * pDevice, ID3D11DeviceContext * pDeviceContext)
 	: CInGameEffect_Model(pDevice, pDeviceContext)
 {
@@ -129,6 +132,7 @@ _int CEffect_Boss_Gravitational_Bomb::Tick(_double TimeDelta)
 
 	Explosion_Check();
 	Scale_Check(TimeDelta);
+	PlayerHit_Check(TimeDelta);
 
 	/* Light */
 	if (m_pLight)
@@ -196,6 +200,41 @@ void CEffect_Boss_Gravitational_Bomb::Scale_Check(_double TimeDelta)
 void CEffect_Boss_Gravitational_Bomb::Explosion_Check()
 {
 	// ¶¥¿¡ ´ê¾Ò´Ù¸é m_IsExplosion = true;
+}
+
+void CEffect_Boss_Gravitational_Bomb::PlayerHit_Check(_double TimeDelta)
+{
+	_float fRange = 5.f;
+
+	_vector vCodyPos = ((CCody*)DATABASE->GetCody())->Get_Transform()->Get_State(CTransform::STATE_POSITION);
+	_vector vMayPos = ((CMay*)DATABASE->GetMay())->Get_Transform()->Get_State(CTransform::STATE_POSITION);
+	_vector vPos = m_pTransformCom->Get_State(CTransform::STATE_POSITION);
+
+	_float vCodyDistance = XMVectorGetX(XMVector3Length(vCodyPos - vPos));
+	_float vMayDistance = XMVectorGetX(XMVector3Length(vMayPos - vPos));
+
+	if (fRange >= vCodyDistance)
+	{
+		m_fCodyHitTime += (_float)TimeDelta;
+
+		if (1.f <= m_fCodyHitTime)
+		{
+			((CCody*)DATABASE->GetCody())->Set_HpBarReduction(10);
+			m_fCodyHitTime = 0.f;
+		}
+	}
+
+	if (fRange >= vMayDistance)
+	{
+		m_fMayHitTime += (_float)TimeDelta;
+
+		if (1.f <= m_fMayHitTime)
+		{
+			((CMay*)DATABASE->GetMay())->Set_HpBarReduction(10);
+			m_fMayHitTime = 0.f;
+		}
+	}
+
 }
 
 CEffect_Boss_Gravitational_Bomb * CEffect_Boss_Gravitational_Bomb::Create(ID3D11Device * pDevice, ID3D11DeviceContext * pDeviceContext, void * pArg)
