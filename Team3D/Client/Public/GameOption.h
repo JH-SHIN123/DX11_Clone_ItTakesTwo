4f1297eeb058824d31680a3c1ea5c2183e07f7ce
DESKTOP-LEIKETP\BLUE <aldald1318@gmail.com>
2021-12-30 16:04:33 +0900

    def

--- Team3D/Client/Public/GameOption.h
+++ Team3D/Client/Public/GameOption.h
@@ -2,11 +2,11 @@
  
 /************************************************************************/
 
-#define __FULLSCREEN
-#define __START_LEVEL_LOGO
-#define __PLAY_CUTSCENE
+//#define __FULLSCREEN
+//#define __START_LEVEL_LOGO
+//#define __PLAY_CUTSCENE
 
-//#define __FIRST_CUTSCENE_OFF
+#define __FIRST_CUTSCENE_OFF
         
 /************************************************************************/
 
@@ -58,7 +58,7 @@
 /**
 * 메이 키보드로 컨트롤하려면 주석 해제.
 */
-//#define __CONTROL_MAY_KEYBOARD
+#define __CONTROL_MAY_KEYBOARD
 
 /************************************************************************/
 
