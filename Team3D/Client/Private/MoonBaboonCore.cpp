ec21e4d05bbba72d2d6e6344c40bf0b2d2766683
DESKTOP-LEIKETP\BLUE <aldald1318@gmail.com>
2021-12-30 22:33:59 +0900

    코어 이펙트 레퍼 삭제

--- Team3D/Client/Private/MoonBaboonCore.cpp
+++ Team3D/Client/Private/MoonBaboonCore.cpp
@@ -46,6 +46,7 @@ HRESULT CMoonBaboonCore::NativeConstruct(void* pArg)
 
 	// Effect
 	FAILED_CHECK_RETURN(m_pGameInstance->Add_GameObject_Clone(Level::LEVEL_STAGE, TEXT("Layer_Effect_BossCore"), Level::LEVEL_STAGE, TEXT("GameObject_2D_Boss_Core"),nullptr, (CGameObject**)&m_pEffectBossCore), E_FAIL);
+	Safe_Release(m_pEffectBossCore);
 
 	DATABASE->Set_MoonBaboonCore(this);
 
@@ -266,7 +267,6 @@ void CMoonBaboonCore::Set_Broken()
 	if (m_pEffectBossCore)
 	{
 		m_pEffectBossCore->HitOn();
-		Safe_Release(m_pEffectBossCore);
 		m_pEffectBossCore = nullptr;
 	}
 
@@ -327,7 +327,7 @@ void CMoonBaboonCore::Free()
 	if (m_pEffectBossCore)
 	{
 		m_pEffectBossCore->Set_Dead();
-		Safe_Release(m_pEffectBossCore);
+		m_pEffectBossCore = nullptr;
 	}
 	Safe_Release(m_pCorePillar);
 	Safe_Release(m_pCoreButton);
